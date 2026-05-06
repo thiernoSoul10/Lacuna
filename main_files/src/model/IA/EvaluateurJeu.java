d7681e50ed1857e9034265409c119390069f40ec
oury <mamadoury20046@gmail.com>
2026-05-06 16:24:35 +0200

    chore:IA

--- main_files/src/model/IA/EvaluateurJeu.java
+++ main_files/src/model/IA/EvaluateurJeu.java
@@ -3,6 +3,9 @@ package model.IA;
 
 // Calcule le score heuristique d'un état
 public class EvaluateurJeu {
+    
+    // Le seuil pour deduire qu'on a la majorité des fleurs d'une couleur
+    private static final int SEUIL_MAJORITE_COULEUR = 4; 
 
     public int evaluer(EtatJeu etat, int indexIA) {
 
@@ -23,11 +26,28 @@ public class EvaluateurJeu {
 
     // Renvoie le nombre de couleurs majoritaires pour un joueur et un état donné
     public int compterCouleursMajoritaires(EtatJeu etat, indexJoueur){
-        return 0;
+        int compteur = 0;
+
+        for (Types.TypeFleur type : Types.TypeFleur.values()) {
+            int nbFleurs = 0;
+            for (Fleur fleur : etat.getFleursParJoueur()[indexJoueur]) {
+                if (fleur.getType() == type) {
+                    nbFleurs++;
+                }
+            }
+
+            if (nbFleurs >= SEUIL_MAJORITE_COULEUR) {
+                compteur++;
+            }
+        }
+
+        return compteur;
+       
     }
 
     // Renvoie le bonus pour départager deux coups de valeures égales
     public int bonus(EtatJeu etat,int  indexIA, int indexJoueur){
+        // À FAIRE ( ON REFLECHIRA SUR ÇA AUSSI )
         return 0;
     }
 
